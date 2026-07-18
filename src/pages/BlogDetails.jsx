13909e3291bf5144fa24d6f257066acc44ff0913
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-19 00:38:57 +0300

    Step 5.3: Fetch single blog details

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function BlogDetails() {
   const { id } = useParams();
 
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((res) => {
+        if (!res.ok) throw new Error("Failed to fetch post");
+        return res.json();
+      })
+      .then((data) => {
+        setPost(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>{error}</h2>;
+
   return (
     <div>
       <h1>Blog Details</h1>
-      <p>Blog ID: {id}</p>
+
+      <h2>{post.title}</h2>
+
+      <p>{post.body}</p>
+
+      <p>
+        <strong>Tags:</strong> {post.tags.join(", ")}
+      </p>
     </div>
   );
 }
