83895540a9debdd110b4e50039d385c0e4ba1e7f
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-19 00:45:26 +0300

    Step 5.4: Fetch and display comments

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -5,13 +5,17 @@ function BlogDetails() {
   const { id } = useParams();
 
   const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // Fetch the selected blog post
     fetch(`https://dummyjson.com/posts/${id}`)
       .then((res) => {
-        if (!res.ok) throw new Error("Failed to fetch post");
+        if (!res.ok) {
+          throw new Error("Failed to fetch post");
+        }
         return res.json();
       })
       .then((data) => {
@@ -22,9 +26,17 @@ function BlogDetails() {
         setError(err.message);
         setLoading(false);
       });
+
+    // Fetch comments for the selected blog post
+    fetch(`https://dummyjson.com/comments/post/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setComments(data.comments);
+      });
   }, [id]);
 
   if (loading) return <h2>Loading...</h2>;
+
   if (error) return <h2>{error}</h2>;
 
   return (
@@ -38,6 +50,16 @@ function BlogDetails() {
       <p>
         <strong>Tags:</strong> {post.tags.join(", ")}
       </p>
+
+      <h3>Comments</h3>
+
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          <h4>{comment.user.username}</h4>
+          <p>{comment.body}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
