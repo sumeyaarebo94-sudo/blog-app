157b2ae2f8314b71563ed180a09a3864f82b20c9
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-19 14:55:03 +0300

    Style global layout and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
+    <nav className="navbar">
       <h2>📝 Personal Blog</h2>
 
-      <Link to="/">Home</Link> |{" "}
-      <Link to="/create">Create Post</Link> |{" "}
-      <Link to="/bookmarks">Bookmarks</Link>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/create">Create Post</Link>
+        <Link to="/bookmarks">Bookmarks</Link>
+      </div>
     </nav>
   );
 }
